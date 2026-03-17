965129a1e971264bbd119005d6e4db4badff379d
Ciberusps <ciberus.ps@gmail.com>
2026-03-17 22:46:04 +0300

    remove UHLConvention

--- Source/ConventionKeeperConventions/Private/ConventionKeeperConventions.cpp
+++ Source/ConventionKeeperConventions/Private/ConventionKeeperConventions.cpp
@@ -1,8 +1,9 @@
 // Pavel Penkov 2025 All Rights Reserved.
 
 #include "ConventionKeeperConventions.h"
+
+#include "ConventionKeeperConvention/ConventionKeeperConvention.h"
 #include "Development/ConventionKeeperSettings.h"
-#include "UHLConvention/UHLConvention.h"
 
 IMPLEMENT_MODULE(FConventionKeeperConventionsModule, ConventionKeeperConventions)
 
@@ -11,7 +12,7 @@ void FConventionKeeperConventionsModule::StartupModule()
 	UConventionKeeperSettings* Settings = GetMutableDefault<UConventionKeeperSettings>();
 	if (Settings && !Settings->Convention)
 	{
-		Settings->Convention = UUHLConvention::StaticClass();
+		Settings->Convention = UConventionKeeperConvention::StaticClass();
 	}
 }
 
